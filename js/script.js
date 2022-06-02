92bfe4c9f47d1025a30a28935a4fcc24edbded0a
Vlad Brazy <shutn1q@gmail.com>
2022-06-02 20:50:35 +0300

    Keep Score

--- js/script.js
+++ js/script.js
@@ -15,17 +15,17 @@ let playerSelection = caseInsensitive(prompt('Rock, Paper or Scissors?'));
 
 function playRound(playerSelection, computerSelection) {
     if (computerSelection === 'Rock' && playerSelection === 'Scissors') {
-        return 'You Lose! Rock beats Scissors';
+        return 'You Lose! Rock crushes Scissors';
     } else if (computerSelection === 'Paper' && playerSelection === 'Rock') {
-        return 'You Lose! Paper beats Rock';
+        return 'You Lose! Paper covers Rock';
     } else if (computerSelection === 'Scissors' && playerSelection === 'Paper') {
-        return 'You Lose! Scissors beats Paper';
+        return 'You Lose! Scissors cuts Paper';
     } else if (computerSelection === 'Scissors' && playerSelection === 'Rock') {
-        return 'You Win! Rock beats Scissors';
+        return 'You Win! Rock crushes Scissors';
     } else if (computerSelection === 'Rock' && playerSelection === 'Paper') {
-        return 'You Win! Paper beats Rock';
+        return 'You Win! Paper covers Rock';
     } else if (computerSelection === 'Paper' && playerSelection === 'Scissors') {
-        return 'You Win! Scissors beats Paper';
+        return 'You Win! Scissors cuts Paper';
     } else if (computerSelection === 'Rock' && playerSelection === 'Rock') {
         return 'Draw!';
     } else if (computerSelection === 'Paper' && playerSelection === 'Paper') {
@@ -40,13 +40,20 @@ function playRound(playerSelection, computerSelection) {
 // Game of 5 Rounds
 
 function game() {
-
+    let playerScore = 0;
+    let computerScore = 0;
     for (let i = 0; i < 5; i++) {
         if (i === 0) {
             console.log('<<< Round One! >>>');
             console.log('Player: ' + playerSelection);
             console.log('Computer: ' + computerSelection);
             console.log(playRound(playerSelection, computerSelection));
+            if (playRound(playerSelection, computerSelection).includes('Win')) {
+                playerScore++;
+            } else if (playRound(playerSelection, computerSelection).includes('Lose')) {
+                computerScore++;
+            } 
+            console.log('Player - ' + playerScore + ' Computer - ' + computerScore);
         } else if (i === 1) {
             playerSelection = caseInsensitive(prompt('Rock, Paper or Scissors?'));
             computerSelection = words[Math.floor(Math.random() * words.length)];
@@ -54,6 +61,12 @@ function game() {
             console.log('Player: ' + playerSelection);
             console.log('Computer: ' + computerSelection);
             console.log(playRound(playerSelection, computerSelection));
+            if (playRound(playerSelection, computerSelection).includes('Win')) {
+                playerScore++;
+            } else if (playRound(playerSelection, computerSelection).includes('Lose')) {
+                computerScore++;
+            } 
+            console.log('Player - ' + playerScore + ' Computer - ' + computerScore);
         } else if (i === 2) {
             playerSelection = caseInsensitive(prompt('Rock, Paper or Scissors?'));
             computerSelection = words[Math.floor(Math.random() * words.length)];
@@ -61,6 +74,12 @@ function game() {
             console.log('Player: ' + playerSelection);
             console.log('Computer: ' + computerSelection);
             console.log(playRound(playerSelection, computerSelection));
+            if (playRound(playerSelection, computerSelection).includes('Win')) {
+                playerScore++;
+            } else if (playRound(playerSelection, computerSelection).includes('Lose')) {
+                computerScore++;
+            } 
+            console.log('Player - ' + playerScore + ' Computer - ' + computerScore);
         } else if (i === 3) {
             playerSelection = caseInsensitive(prompt('Rock, Paper or Scissors?'));
             computerSelection = words[Math.floor(Math.random() * words.length)];
@@ -68,6 +87,12 @@ function game() {
             console.log('Player: ' + playerSelection);
             console.log('Computer: ' + computerSelection);
             console.log(playRound(playerSelection, computerSelection));
+            if (playRound(playerSelection, computerSelection).includes('Win')) {
+                playerScore++;
+            } else if (playRound(playerSelection, computerSelection).includes('Lose')) {
+                computerScore++;
+            } 
+            console.log('Player - ' + playerScore + ' Computer - ' + computerScore);
         } else if (i === 4) {
             playerSelection = caseInsensitive(prompt('Rock, Paper or Scissors?'));
             computerSelection = words[Math.floor(Math.random() * words.length)];
@@ -75,6 +100,19 @@ function game() {
             console.log('Player: ' + playerSelection);
             console.log('Computer: ' + computerSelection);
             console.log(playRound(playerSelection, computerSelection));
+            if (playRound(playerSelection, computerSelection).includes('Win')) {
+                playerScore++;
+            } else if (playRound(playerSelection, computerSelection).includes('Lose')) {
+                computerScore++;
+            } 
+            console.log('Player - ' + playerScore + ' Computer - ' + computerScore);
+            if (playerScore > computerScore) {
+                console.log('You Won! ' + playerScore + " - " + computerScore);
+            } else if (playerScore < computerScore) {
+                console.log('You Lost! ' + playerScore + " - " + computerScore);
+            } else {
+                console.log('Tie! ' + playerScore + " - " + computerScore);
+            }
         }
     }
 }
